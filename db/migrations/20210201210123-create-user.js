5689759f0890d5170ad40077eca4a1589c0a10ea
Alana <alana.lapoint@gmail.com>
2021-02-01 17:49:41 -0500

    setup db, formvalidators, user signup route, etc

---
+++ db/migrations/20210201210123-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+        unique: true
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(70)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator')
+const { User } = require('../db/models');
+const asyncHandler = (handler) => {
+  return (req, res, next) => {
+    handler(req, res, next).catch(next);
+  }
+}
+
+const formValidator = [
+  check('email')
+    .custom(async email => {
+      await User.findOne({
+        where: {
+          email: email
+        }
+      }).then(isEmail => {
+        if (isEmail) {
+          return Promise.reject('Email is already in use');
+        }
+      })
+    })
+    .withMessage('Email is already in use')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  check('confirmPassword')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Password confirmation does not match password');
+      }
+      return true;
+    })
+    .withMessage('Password confimation does not match password')
+];
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', csrfProtection, (req, res, next) => {
+  res.render('create-user', { csrfToken: req.csrfToken() })
 });
+
+router.post('/', formValidator, csrfProtection, asyncHandler(async (req, res) => {
+  const { email, name, password, confirmPassword } = req.body;
+  const validatorErrors = validationResult(req);
+  if (validatorErrors.isEmpty()) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({ email, name, hashedPassword });
+    res.render('profile');
+  }
+  else {
+    const errors = validatorErrors.array().map((error) => error.msg)
+    res.render('create-user', { email, name, errors, csrfToken: req.csrfToken() })
+  }
+}))
 
 module.exports = router;
