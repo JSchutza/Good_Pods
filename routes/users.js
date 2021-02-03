e343f96150cbf0174f045f07a82adcd01572336d
Alana <alana.lapoint@gmail.com>
2021-02-03 16:59:52 -0500

    began form-styling, refactored users.js

--- routes/users.js
+++ routes/users.js
@@ -1,75 +1,103 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../db/models');
-const { csrf, csrfProtection, bcrypt, check, validationResult, asyncHandler } = require("../lib/util")
-// const formValidator = [
-//   check('email')
-//     .exists({ checkFalsy: true })
-//     .withMessage('Please provide an email address.')
-//     .isLength({ max: 255 })
-//     .withMessage('Whoaaa that\'s a long email! Try again.')
-//     .custom(async email => {
-//       await User.findOne({
-//         where: {
-//           email: email
-//         }
-//       }).then(isEmail => {
-//         if (isEmail) {
-//           return Promise.reject('Email is already in use');
-//         }
-//       })
-//     })
-//     .withMessage('Email is already in use')
-//     .isEmail()
-//     .withMessage('Email must be valid'),
-//   check('name')
-//     .exists({ checkFalsy: true })
-//     .withMessage('Please enter your name.')
-//     .isLength({ max: 70 })
-//     .withMessage('Your name is too long!'),
-//   check('password')
-//     .exists({ checkFalsy: true })
-//     .withMessage('Please enter a password.'),
-//   check('confirmPassword')
-//     .custom((value, { req }) => {
-//       if (value !== req.body.password) {
-//         throw new Error('Password confirmation does not match password');
-//       }
-//       return true;
-//     })
-//     .withMessage('Password confimation does not match password')
-// ];
+const { csrf, csrfProtection, bcrypt, check, validationResult, asyncHandler, createShelves } = require("../lib/util")
+const { loginUser, logoutUser } = require("../auth")
+const loginValidators = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Please enter your email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('You forgot to enter your password!')
+];
+
+const signUpValidator = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an email address.')
+        .isLength({ max: 255 })
+        .withMessage('Whoaaa that\'s a long email! Try again.')
+        .custom(async email => {
+            await User.findOne({
+                where: {
+                    email: email
+                }
+            }).then(isEmail => {
+                if (isEmail) {
+                    return Promise.reject('Email is already in use');
+                }
+            })
+        })
+        .withMessage('Email is already in use')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Please enter your name.')
+        .isLength({ max: 70 })
+        .withMessage('Your name is too long!'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please enter a password.'),
+    check('confirmPassword')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password confirmation does not match password');
+            }
+            return true;
+        })
+        .withMessage('Password confimation does not match password')
+];
 
 
-
-/* GET users listing. */
-
-
-
-
-
-// router.get('/', csrfProtection, (req, res, next) => {
-//   res.render('create-user', { csrfToken: req.csrfToken() })
-// });
-
-// router.post('/', formValidator, csrfProtection, asyncHandler(async (req, res) => {
-//   const { email, name, password, confirmPassword } = req.body;
-//   const validatorErrors = validationResult(req);
-//   if (validatorErrors.isEmpty()) {
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = await User.create({ email, name, hashedPassword });
-//     req.session.user = user;
-
-//     return req.session.save(() => {
-//       res.render('profile');
-//     })
+router.post('/', signUpValidator, csrfProtection, asyncHandler(async (req, res) => {
+    const { email, name, password, confirmPassword } = req.body;
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty()) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ email, name, hashedPassword });
+        createShelves(user)
+        loginUser(req, res, user)
+        return req.session.save(() => {
+            res.render('profile');
+        })
+    }
+    else {
+        const errors = validatorErrors.array().map((error) => error.msg)
+        res.render('create-user', { email, name, errors, csrfToken: req.csrfToken() })
+    }
+}))
 
 
-//   }
-//   else {
-//     const errors = validatorErrors.array().map((error) => error.msg)
-//     res.render('create-user', { email, name, errors, csrfToken: req.csrfToken() })
-//   }
-// }))
+router.get("/login", csrfProtection, (req, res) => {
+    res.render("login", { csrfToken: req.csrfToken() })
+})
+
+router.post("/login", csrfProtection, loginValidators, asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
+
+    const RealPassword = user.hashedPassword.toString()
+
+    const passwordMatch = await bcrypt.compare(password, RealPassword)
+    const validatorErrors = validationResult(req);
+    const errors = validatorErrors.array().map((error) => error.msg);
+    if (passwordMatch && validatorErrors.isEmpty()) {
+        loginUser(req, res, user)
+        return req.session.save(() => {
+            res.render("profile")
+        })
+    } else {
+        res.render('login', { errors, csrfToken: req.csrfToken(), email })
+    }
+}))
+
+router.post('/logout', (req, res) => {
+    logoutUser(req, res);
+    res.redirect("/")
+})
+
 
 module.exports = router;
+
