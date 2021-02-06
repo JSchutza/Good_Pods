293cd52fb5f7dddefa7d5be7f9f3cc031be72ea0
Caleb <cpbeachler@gmail.com>
2021-02-05 20:43:27 -0500

    seeder Data lookin good

--- public/javascripts/podcast.js
+++ public/javascripts/podcast.js
@@ -1,19 +1,3 @@
-
-  console.log('this works')
-const shelfButtons = document.querySelectorAll(".shelf_btn")
-shelfButtons.addEventListener("click",async (event) => {
-  console.log('this works')
-  const shelfButton = event.target.id
-  await fetch("/api/shelves", {
-    method: "POST",
-    headers: {
-      "Content-Type" : "application/json"
-    },
-    body: JSON.stringify({"podcastId": 1, "shelfType": `${shelfButton}`})
-
-  })
-})
-
 window.addEventListener('DOMContentLoaded', () => {
   const reviewArea = document.getElementById('ReviewDiv')
 
@@ -69,5 +53,3 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
 })
-
-
