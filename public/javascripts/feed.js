491e29718903c484fa52b472d9356fa0b80a9872
OByrnes <liviabyrnes@gmail.com>
2021-02-05 21:23:20 -0500

    added some stuff

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -3,15 +3,15 @@ window.addEventListener('DOMContentLoaded', ()=> {
   
   const featuredPodList = featuredPods()
   const header = document.createElement("h2")
-  featuredPodList.forEach( pod => {
-    const featPodDiv = document.createElement('div')
-    const featPodimg = document.createElement("img")
-    const featPod= document.createElement('a')
-    featPodimg.setAttribute('src', `images/catalog/${pod.id}.jpeg`)
-    featPod.setAttribute('href', `/podcasts/${pod.id}`)
-    featPodDiv.appendChild(featPodimg)
-    featPodDiv.appendChild()
-  })
+  // featuredPodList.forEach( pod => {
+  //   const featPodDiv = document.createElement('div')
+  //   const featPodimg = document.createElement("img")
+  //   const featPod= document.createElement('a')
+  //   featPodimg.setAttribute('src', `images/catalog/${pod.id}.jpeg`)
+  //   featPod.setAttribute('href', `/podcasts/${pod.id}`)
+  //   featPodDiv.appendChild(featPodimg)
+  //   featPodDiv.appendChild()
+  // })
 
   
 })
@@ -20,7 +20,7 @@ const featuredPods = async() => {
   const resJson = await res.json()
   if (res.ok) {
     const featuredPods = []
-    for (let i =0; i < 5, i++){
+    for (let i =0; i < 5; i++){
       featuredPods.push(resJson[Math.floor(Math.random()* resJson.length)])
     }
   }
